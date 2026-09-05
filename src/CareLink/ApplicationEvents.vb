58c54aaeb2fa62940ed17aea046831861dd7d623
Clyferf <114229198+AlexFLAVIUS35@users.noreply.github.com>
2026-09-05 18:47:07 +0300

    Handle CareLink OAuth protocol activation

--- src/CareLink/ApplicationEvents.vb
+++ src/CareLink/ApplicationEvents.vb
@@ -6,27 +6,26 @@ Imports Microsoft.VisualBasic.ApplicationServices
 
 Namespace My
 
-    ' The following events are available for MyApplication:
-    ' Startup: Raised when the application starts, before the startup form is created.
-    ' Shutdown: Raised after all application forms are closed.
-    '           This event is not raised if the application terminates abnormally.
-    ' StartupNextInstance: Raised when launching a single-instance application and the application is already active.
-    ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
 
         Private Sub Me_ApplyApplicationDefaults(sender As Object, e As ApplyApplicationDefaultsEventArgs) _
             Handles Me.ApplyApplicationDefaults
 
-            ' Setting the application-wide default Font:
-            ' e.Font = New Font(FontFamily.GenericSansSerif, 9, FontStyle.Regular)
-
-            ' Setting the HighDpiMode for the Application:
             e.HighDpiMode = HighDpiMode.PerMonitorV2
             e.ColorMode = SystemColorMode.Dark
             e.FormRevealMode = FormRevealMode.Deferred
-            '    ' If a splash dialog is used, this sets the minimum display time:
-            '    e.MinimumSplashScreenDisplayTime = 4000
             e.VisualStylesMode = VisualStylesMode.Net11
+        End Sub
+
+        Private Sub Me_StartupNextInstance(sender As Object, e As StartupNextInstanceEventArgs) _
+            Handles Me.StartupNextInstance
+
+            For Each argument As String In e.CommandLine
+                If argument.StartsWith("com.medtronic.carepartner:", StringComparison.OrdinalIgnoreCase) Then
+                    DefaultBrowserOAuth.HandleProtocolCallback(argument)
+                    Exit For
+                End If
+            Next
         End Sub
 
         Private Sub Me_UnhandledException(sender As Object, e As UnhandledExceptionEventArgs) _
